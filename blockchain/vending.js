f10b1f16406c632dfac1408e45194c6e0f8e2080
shan2001-s <sshan02001@gmail.com>
2022-06-11 20:02:54 +0630

    add styles and function

--- blockchain/vending.js
+++ blockchain/vending.js
@@ -15,8 +15,11 @@
 
 const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 
-const VendingMechingContract = web3=>{
- return  new web3.eth.Contract(abi,"0xe1886A5BD02D49F4f8c0A236E69F67D52628bC9c")
+const VendingMechingContract = web3 => {
+ return  new web3.eth.Contract(
+     abi,
+     "0xe1886A5BD02D49F4f8c0A236E69F67D52628bC9c"
+ )
 
 }
 
